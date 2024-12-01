9af9f6bf1ae74cd07f811f7fadad94616ca1baa8
ungra <ungrarhythm@naver.com>
2024-12-01 20:38:19 +0900

    bugfix

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -16,7 +16,7 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get()
+  @Get('search')
   search(@Query('year') searchingYear: string) {
     return `We are searching for a movie made after: ${searchingYear}`;
   }
